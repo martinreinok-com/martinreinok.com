dc782db06dc55385ed96acfd91ad5fd91ad0a7da
Martin Reinok <m.reinok@student.utwente.nl>
2024-02-12 00:32:40 +0100

    Reworked thumbnail system to automatic. Made Render portfolio nicer

--- js/foldergallery.shortcode.js
+++ js/foldergallery.shortcode.js
@@ -1,18 +1,40 @@
 const fs = require('fs');
 const sharp = require('sharp');
+const path = require('path');
+let lightbox = require('./lightboxref.shortcode');
 
 module.exports = function (folderPath) {
     const folder_data = fs.readdirSync(`.${folderPath}`, { withFileTypes: true });
+    const thumbnailFolderPath = `.${folderPath}/thumbnail`;
+    const highresFolderPath = `.${folderPath}`;
+
+    if (!fs.existsSync(thumbnailFolderPath)) {
+        fs.mkdirSync(thumbnailFolderPath);
+    }
+
     const images = folder_data
         .filter(dirent => dirent.isFile())
         .map(dirent => dirent.name);
     let galleryHTML = '<div class="foldergallery">';
     
     images.forEach(image => {
-    linkOpen = `<a href="${folderPath}highres/${image}" target="_blank">`;
-    linkClose = `</a>`;
-    galleryHTML += `
-    <figure>${linkOpen}<img src="${folderPath}/${image}" class="foldergallery-img" loading="lazy"/>${linkClose}<figcaption></figcaption></figure>`;
+        const thumbnailImagePath = path.join(thumbnailFolderPath, image);
+        const imagePath = path.join(highresFolderPath, image);
+
+        if (!fs.existsSync(thumbnailImagePath)) {
+            console.log("Creating thumbnail:", thumbnailImagePath);
+            sharp(imagePath)
+                .resize({ width: 500 })
+                .toFile(thumbnailImagePath, (err, info) => {
+                    if (err) {
+                        console.error('Error generating thumbnail:', err);
+                    }
+                });
+        }
+        linkOpen = `<a href="${folderPath}/${image}">`;
+        linkClose = `</a>`;
+        galleryHTML += `
+        <figure>${linkOpen}<img src="${folderPath}/thumbnail/${image}" class="foldergallery-img" loading="lazy"/>${linkClose}<figcaption></figcaption></figure>`;
     });
     
     galleryHTML += '</div>';
--- misc/3drender/index.md
+++ misc/3drender/index.md
@@ -1,16 +1,15 @@
 ---
 layout: header-footer-page.njk
-width: 100%
+width: 70%
 ---
 
-<html>
-    <body>
-        <div class="column-container">
-        <div class="column">
-            <section class="gallery">
-            {% foldergallery "/media/misc/3drender/" %}
-            </section>
-        </div>
-        </div>
-    </body>
-</html>
+# Product Renders
+{% foldergallery "/media/misc/3drender/products" %}
+
+# Cover art
+Single cover art for music
+{% foldergallery "/media/misc/3drender/cover-art" %}
+
+# Showcase
+Just designs I did for fun and to try out new techniques
+{% foldergallery "/media/misc/3drender/misc" %}
